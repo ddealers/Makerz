1eaf19b4788e951ae92152ad178275e1950c44da
vampaynani <vampay@digitaldealers.mx>
2015-05-23 23:04:26 -0500

    new form for course request

--- js/main.js
+++ js/main.js
@@ -91,6 +91,62 @@ $(document).on('ready', function(){
 		})
 		return false;	
 	});
+// M A I L  I N S C R I P C I Ó N//
+	TweenLite.set('#subscribe-form', {css:{autoAlpha: 0}});
+	TweenLite.set('#subscribe-form form', {y: -100});
+	$('#subscribe-btn').on('click', function(e){
+		e.preventDefault();
+		TweenLite.to('#subscribe-form', 0.7, {css:{autoAlpha: 1}, ease: Power2.easeOut});
+		TweenLite.to('#subscribe-form form', 0.7, {y: 0, ease: Back.easeOut});
+	});
+	$('.fill-back').on('click', function(e){
+		e.preventDefault();
+		TweenLite.to('#subscribe-form', 0.7, {css:{autoAlpha: 0}, ease: Power2.easeOut});
+		TweenLite.to('#subscribe-form form', 0.7, {y: -100, ease: Back.easeIn});
+	});
+	$('#subscribe-me').submit(function(e){
+		var $form			= $(this);
+		var name 			= $("#subscribe-name").val();
+		var email 			= $("#subscribe-email").val();
+		var phone 			= $("#subscribe-phone").val();
+		var course 			= $("#subscribe-course").val();
+		var message 		= $("#subscribe-message").val();
+		var emailIsString 	= /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm.test(email);
+		
+		if(!email|| !emailIsString){
+			alert("El dato E-mail no es válido");
+			return false;
+		}
+		
+		$form.find('.btn').prop('disabled', true);
+		var base64email = "dmFtcGF5QGRpZ2l0YWxkZWFsZXJzLm14";
+		var baseURL = "//formspree.io/";
+		$.ajax({
+			url: baseURL + atob(base64email),
+			method: "POST",
+			data: {name:name, email: email, phone: phone, course:course, message: message, _subject: "MAKERZ :: " + name + " desea inscribirse a " + course },
+			dataType: "json",
+			success: function(){
+				var $p = $("<p>Tu correo ha sido enviado y será contestado a la brevedad ¡Buen día!</p>");
+				$form.prepend($p);
+				$p.delay(3000).fadeOut(400, function(){
+					TweenLite.to('#subscribe-form', 0.7, {css:{autoAlpha: 0}, ease: Power2.easeOut});
+					TweenLite.to('#subscribe-form form', 0.7, {y: -100, ease: Back.easeIn});
+					$("#subscribe-name").val('');
+					$("#subscribe-email").val('');
+					$("#subscribe-phone").val('');
+					$("#subscribe-message").val('');
+					$p.remove();
+				});
+				$form.find('.btn').prop('disabled', false);
+				
+			},
+			error: function(){
+				alert("No fue posible enviar tu correo, inténtalo nuevamente.");
+			}
+		});
+		return false;	
+	});
 });
 
 
